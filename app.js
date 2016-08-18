f5abf748de464a199d060d5b570a78b3493628ba
Prajwal Joshi <prazwal@outlook.com>
2016-08-18 12:10:06 +1000

    add new blog feature

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 var express = require("express");
-
 var app = express();
+var bodyParser = require("body-parser");
 
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
 
+var blogs = [
+  {title: "TALES FROM THE ROAD", image:"http://www.child.com.au/resources/site/child/blog/blog-56-intro.jpg"},
+  {title: "LAKESHORE MAGNETIC FISHING SET", image:"http://www.child.com.au/resources/site/child/blog/blog-55-intro.jpg"},
+  {title: "SPEECH SOUNDS", image:"http://www.child.com.au/resources/site/child/blog/blog-54-intro.jpg"}
+]
+
 app.get("/", function(req,res){
-        res.render("cmshome");
+        res.render("cmshome.ejs");
 });
+
 
 
 app.get("/blogs", function(req,res){
 
-      var blogs = [
-        {title: "TALES FROM THE ROAD", image:"http://www.child.com.au/resources/site/child/blog/blog-56-intro.jpg"},
-        {title: "LAKESHORE MAGNETIC FISHING SET", image:"http://www.child.com.au/resources/site/child/blog/blog-55-intro.jpg"},
-        {title: "SPEECH SOUNDS", image:"http://www.child.com.au/resources/site/child/blog/blog-54-intro.jpg"}
-      ]
-        res.render("blogs", { blogs: blogs});
+
+        res.render("blogs.ejs", { blogs: blogs});
+});
+
+app.post("/blogs", function(req, res){
+    console.log("we are here");
+    //get data from form
+    var blogTitle = req.body.title;
+    var blogImage = req.body.image;
+    //add to db
+    var newBlog = {title: blogTitle, image:blogImage}
+    blogs.push(newBlog);
+    //redirect to blogs
+
+    res.redirect("/blogs");
+
+});
+
+app.get("/blogs/new", function(req,res){
+    res.render("newblog.ejs")
 });
 
 app.listen(3000, function(){
